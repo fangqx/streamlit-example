ca1e26c16cfc44269e52570552e2b7eb8e075805
Quanxiao Fang <31942795+fangqx@users.noreply.github.com>
2023-12-06 17:49:00 +0800

    Update streamlit_app.py
    tt

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,9 +17,9 @@ In the meantime, below is an example of what you can do with just a few lines of
 
 # Everything is accessible via the st.secrets dict:
 
-st.write("DB username:", st.secrets["db_username"])
-st.write("DB password:", st.secrets["db_password"])
-st.write("My cool secrets:", st.secrets["my_cool_secrets"]["things_i_like"])
+#st.write("DB username:", st.secrets["db_username"])
+#st.write("DB password:", st.secrets["db_password"])
+#st.write("My cool secrets:", st.secrets["my_cool_secrets"]["things_i_like"])
 
 # And the root-level secrets are also accessible as environment variables:
 
